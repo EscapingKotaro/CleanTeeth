553a39ca9c9602e82b8092af2c4cc02e498e7efe
EscapingKotaro <kokorinleonid83@gmail.com>
2026-09-23 14:29:16 +0300

    my 2309 work 2

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("crm/", include("crm.urls")),
     path("", include("crm.urls")),  # корень тоже ведёт в CRM
 ]
 
